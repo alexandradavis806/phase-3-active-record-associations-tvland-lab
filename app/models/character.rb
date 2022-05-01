2a1ba7c0e2573be3f8cc64b1ebd87039d7ef50eb
Alexandra Davis <alex.davis806@gmail.com>
2022-05-01 13:21:51 -0700

    commit tree 22ef745dc2e19cf6618608f66ea02e4d1014b5ba

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,15 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :actor
+  belongs_to :show
+
+  def actor_instance 
+    self.actor
+  end
+
+  def show_instance
+    self.show
+  end 
+  def say_that_thing_you_say
+    "#{self.name} always says: #{self.catchphrase}"
+  end 
 end
